4651f7040d67ddfbce4a3b8ae0bd01bbce310689
DrCausa <leo10said@gmail.com>
2025-10-28 12:39:32 -0500

    fix(tooltip): update tooltip arrow color for better visibility

--- packages/frontend/src/components/commons/Tooltip/index.tsx
+++ packages/frontend/src/components/commons/Tooltip/index.tsx
@@ -17,7 +17,7 @@ const Tooltip = ({ children, message, className = "" }: TooltipProps) => {
         `)}
       >
         <div className="peer">{children}</div>
-        <div className="z-1000 hidden absolute text-[10px] w-[8rem] left-[50%] -translate-x-[50%] bottom-7 bg-bg-surface-dark dark:bg-bg-surface-light dark:text-text-secondary-light text-text-secondary-dark px-2 py-1 rounded-lg text-center peer-hover:block after:absolute after:left-[50%] after:-translate-x-[50%] after:bottom-0 after:translate-y-3.5 after:text-white after:text-[15px] after:content-['▼'] after:pointer-events-none">
+        <div className="z-1000 hidden absolute text-[10px] w-[8rem] left-[50%] -translate-x-[50%] bottom-7 bg-bg-surface-dark dark:bg-bg-surface-light dark:text-text-secondary-light text-text-secondary-dark px-2 py-1 rounded-lg text-center peer-hover:block after:absolute after:left-[50%] after:-translate-x-[50%] after:bottom-0 after:translate-y-3.5 after:text-bg-surface-dark dark:after:text-bg-surface-light after:text-[15px] after:content-['▼'] after:pointer-events-none">
           {message}
         </div>
       </div>
